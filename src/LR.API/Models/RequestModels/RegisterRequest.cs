6be822b49512e1dbd5d8d9c6bbea307aa8bca570
s-peftev <s.peftev@gmail.com>
2025-08-06 12:34:22 +0300

    fix: review comments

--- src/LR.API/Models/RequestModels/RegisterRequest.cs
+++ src/LR.API/Models/RequestModels/RegisterRequest.cs
@@ -7,17 +7,13 @@ namespace LR.API.Models.RequestModels
         [Required]
         [StringLength(20, MinimumLength = 4)]
         public string? UserName { get; set; }
-
         [Required]
         public string? FirstName { get; set; }
-
         [Required]
         public string? LastName { get; set; }
-
         [Required]
         [EmailAddress]
         public string? Email { get; set; }
-
         [Required]
         [StringLength(20, MinimumLength = 4)]
         public string? Password { get; set; }
